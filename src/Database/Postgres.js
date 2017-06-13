0d9fc251aef9cf55bde7f51dea46d5986a7557be
FrigoEU <simon.van.casteren@gmail.com>
2017-06-13 12:46:57 +0200

    Fixed foreign diagnostics func

--- src/Database/Postgres.js
+++ src/Database/Postgres.js
@@ -113,8 +113,7 @@ exports.isObjectWithAllNulls = function(fn){
 };
 
 exports.showDiagnostics = function(rawResult){
-  function(row){
-    delete rawResult.rows;
+  return function(row){
     return ["Problem row: " + JSON.stringify(row)]
       .concat( _.map(rawResult.fields, function(f){return JSON.stringify(f)}))
       .concat(["check https://github.com/brianc/node-pg-types/blob/master/lib/textParsers.js for info on parsers"])
